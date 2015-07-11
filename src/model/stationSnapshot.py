1bf2d36f164d6d93e47643a686177cb3856ae4d7
nicgirault <nic.girault@gmail.com>
2015-07-11 18:38:19 +0200

    add indexes

--- src/model/stationSnapshot.py
+++ src/model/stationSnapshot.py
@@ -5,13 +5,13 @@ from datetime import datetime
 class StationSnapshot(db.Model, BaseModel):
 
     id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.Integer, nullable=False)
-    contract_name = db.Column(db.String(120), nullable=False)
+    number = db.Column(db.Integer, nullable=False, index=True)
+    contract_name = db.Column(db.String(120), nullable=False, index=True)
     status = db.Column(db.String(20), nullable=False)
     bike_stands = db.Column(db.Integer, nullable=False)
     available_bike_stands = db.Column(db.Integer, nullable=False)
     available_bikes = db.Column(db.Integer, nullable=False)
-    last_update = db.Column(db.DateTime, nullable=False)
+    last_update = db.Column(db.DateTime, nullable=False, index=True)
 
     def __init__(self, stationData):
         self.number = stationData.get('number')
